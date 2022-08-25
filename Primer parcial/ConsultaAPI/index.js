3bbd22cb2b3800e2a8897f5d127bb3c0513abcd4
Alan Peña <alancin7@live.com.mx>
2022-08-25 08:27:14 -0500

    code: Primera tarea primer parcial

---
+++ Primer parcial/ConsultaAPI/index.js
@@ -0,0 +1,13 @@
+//Import fetch
+// Use node.js para ejecutar el codigo
+const fetch = require('node-fetch');
+const url = 'http://dog-api.kinduff.com/api/facts';
+
+(async()=>{
+    const res = await fetch(url);
+    const data = await res.json();
+    const { facts } = data;
+    
+    console.info(facts);
+
+})();
