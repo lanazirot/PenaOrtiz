42bcca126b0722906bd2f8a0ada70ea6c72523e6
Alan Peña Ortiz <alancin7@live.com.mx>
2022-08-28 19:46:23 -0500

    code: movimientos de archivos fetching

--- Primer parcial/ConsultaAPI/index.js
+++ Primer parcial/ConsultaAPI/index.js
@@ -1,6 +1,6 @@
 //Import fetch
-// Use node.js para ejecutar el codigo
 const fetch = require('node-fetch');
+// Use node.js para ejecutar el codigo
 const url = 'http://dog-api.kinduff.com/api/facts';
 
 
