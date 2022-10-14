4bf877c1566e17bfb2d6526de77996ad124c3ecf
Alan Peña Ortiz <alancin7@live.com.mx>
2022-10-13 21:37:37 -0500

    code: json 2 excel

--- Segundo parcial/SQL/index.js
+++ Segundo parcial/SQL/index.js
@@ -1,9 +1,10 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const sql = require("mssql");
 const express = require("express");
-const cors = require("cors")
-const routerApi = require('./routes/index.js');
+const cors = require("cors");
+const routerApi = require("./routes/index.js");
+
 const appPool = new sql.ConnectionPool(process.env.CONNECTION);
 
 const app = express();
@@ -12,7 +13,7 @@ const port = process.env.PORT || 3000;
 appPool
   .connect()
   .then((pool) => {
-    routerApi(app)
+    routerApi(app);
     app.locals.db = pool;
     app.use(cors({ origin: "*" }));
     app.listen(port);
--- Segundo parcial/SQL/routes/index.js
+++ Segundo parcial/SQL/routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const productsRouter = require("./customersRoute.route");
+const json2xls = require("json2xls");
 
 const routerApi = app => {
   const router = express.Router();
   app.use('/api', router);
+  router.use(json2xls.middleware)
   router.use('/customers', productsRouter);
 }
 module.exports = routerApi;
